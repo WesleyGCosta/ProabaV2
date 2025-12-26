66c4105c58c0dc4db78d9f71dfe9d486fd011ea7
WesleyGCosta <Wesleygomescosta825@gmail.com>
2025-12-26 14:48:28 -0300

    Adicionando arquivos de variaveis e mudando de css para scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './layouts/auth-component/auth-component';
+import { LoginComponent } from './views/auth/login-component/login-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    {  
+        path: '',
+        component: AuthComponent,
+        children: [
+            { path: 'login', component: LoginComponent }
+        ]
+    },
+];
