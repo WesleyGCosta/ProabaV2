fa6e7d9f75a1492cac80918040a8458b9faf3d41
WesleyGCosta <Wesleygomescosta825@gmail.com>
2025-12-27 16:14:04 -0300

    adicionando primeNG no projeto e montando a tela de login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    providePrimeNG({
+            theme: {
+                preset: Aura,
+                options: {
+                  primary: 'blue',
+                  scale: 0.9,
+                  darkModeSelector: '.my-app-dark'
+                }
+            }
+        }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
--- src/app/views/auth/login-component/login-component.ts
+++ src/app/views/auth/login-component/login-component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { Checkbox } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+   imports: [FormsModule, InputTextModule, ButtonModule, Checkbox],
+  providers: [PasswordModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.scss',
 })
 export class LoginComponent {
 
+  protected email: string | undefined;
+  protected password: string | undefined;
+  protected remenber = false;
 }
