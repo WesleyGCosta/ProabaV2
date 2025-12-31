f45f0c24fa7f8681aa654d3a212d99da76538604
WesleyGCosta <Wesleygomescosta825@gmail.com>
2025-12-31 12:42:48 -0300

    Adicionando responsividade na tela de login

---
+++ src/app/models/login-request.ts
@@ -0,0 +1,24 @@
+import { email, required, schema } from "@angular/forms/signals"
+
+export interface LoginRequest {
+    email: string,
+    password: string,
+    remenber: boolean
+}
+
+export const initialDataLogin: LoginRequest = {
+    email: '',
+    password: '',
+    remenber: false
+}
+
+export const loginValidations = schema<LoginRequest>((path) => {
+    required(path.email);
+    email(path.email, { message: 'E-mail inválido' });
+    required(path.password);
+})
+
+export enum typeForm {
+    login,
+    forgot
+}
--- src/app/views/auth/login-component/login-component.ts
+++ src/app/views/auth/login-component/login-component.ts
@@ -1,22 +1,88 @@
-import { Component} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, computed, effect, inject, OnInit, signal } from '@angular/core';
+import { initialDataLogin, LoginRequest, loginValidations, typeForm, typeForm as typeFormLogin } from '../../../models/login-request';
 import { InputTextModule } from 'primeng/inputtext';
-import { PasswordModule } from 'primeng/password';
-import { ButtonModule } from 'primeng/button';
+import { Field, form } from '@angular/forms/signals';
+import { Button } from 'primeng/button';
 import { Checkbox } from 'primeng/checkbox';
-import { InputGroupModule } from 'primeng/inputgroup';
-import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputGroupAddon } from 'primeng/inputgroupaddon';
+import { InputGroup } from 'primeng/inputgroup';
+import { NgOptimizedImage } from '@angular/common';
+import { PasswordModule } from 'primeng/password';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login-component',
-   imports: [FormsModule, InputTextModule, ButtonModule, Checkbox, InputGroupModule, InputGroupAddonModule],
+  imports: [Button, Checkbox, InputGroupAddon, InputGroup, Field, NgOptimizedImage, InputTextModule],
   providers: [PasswordModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  protected email: string | undefined;
-  protected password: string | undefined;
-  protected remenber = false;
+  private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+
+  protected loginRequestModel = signal<LoginRequest>(initialDataLogin);
+  protected loginRequestForm = form(this.loginRequestModel, loginValidations);
+  protected isInvalid = computed(() => this.loginRequestForm().invalid());
+  protected mode = signal<typeFormLogin>(typeFormLogin.login);
+  protected eTypeForm = typeForm;
+  protected titleForm = computed(() =>
+    this.mode() === typeForm.login
+      ? 'Bem-vindo ao PROABA'
+      : 'Digite seu e-mail para redefinir sua senha'
+  );
+  protected bannerClass = computed(() =>
+    this.mode() === typeForm.login
+      ? 'welcome'
+      : 'forgot'
+  );
+
+  ngOnInit(): void {
+    const modeParam = this.activatedRoute.snapshot.queryParamMap.get('mode');
+
+    if (modeParam === 'forgot') {
+      this.mode.set(typeForm.forgot);
+    } else {
+      this.mode.set(typeForm.login);
+    }
+  }
+
+  submitted() {
+    const formInstance = this.loginRequestForm();
+
+    if (formInstance.invalid()) {
+      formInstance.markAsTouched();
+      return;
+    }
+    console.log(formInstance.value())
+  }
+
+  protected canSubmit = computed(() => {
+    const form = this.loginRequestForm();
+
+    if (this.mode() === this.eTypeForm.login) {
+      return form.valid();
+    }
+
+    return this.loginRequestForm.email().valid();
+  });
+
+
+  toggleTypeForm() {
+    const form = this.loginRequestForm();
+    form.reset()
+    this.loginRequestModel.set({ ...initialDataLogin });
+    this.mode.update(current =>
+      current === typeForm.login ? typeForm.forgot : typeForm.login
+    );
+
+    const isForgotMode = this.mode() === typeForm.forgot;
+    this.router.navigate([], {
+      queryParams: { mode: isForgotMode ? 'forgot' : null },
+      queryParamsHandling: 'merge'
+    })
+  }
+
 }
